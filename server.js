79bc392ee4ee5814a318825d390d2d16a672528e
Anikesh99 <b.anikesh@gmail.com>
2020-12-19 19:43:08 +0800

    Make the website production ready

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const YoutubeMP3Downloader = require('youtube-mp3-downloader')
 
 const port = process.env.PORT || 4000;
 
-app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
-}))
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 app.listen(port, () => {
     console.log('Server works!!! At port 4000')
 })
